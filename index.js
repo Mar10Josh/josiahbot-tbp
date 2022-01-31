987feed24676bbc728f12041d6f1dc78dfb6e37c
Mar10Josh <65928867+Mar10Josh@users.noreply.github.com>
2022-01-30 19:07:05 -1000

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ client.on('message', function (data) {
                  client.send("Farewell i guess")
                  process.exit(0)
              }
-         } else if (msg == "j!uptime") {
+         } else if (msg == pfx + "uptime") {
            client.send("Seconds of Github VM OS (Ubuntu Latest):\n|_ " + tocsp(os.uptime()) + "\nSeconds of me (a process)\n|_" + tocsp(process.uptime()))
          }
          else {
